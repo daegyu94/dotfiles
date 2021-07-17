6964d7b6ebef68a926cda0a19dcb208ff858c7cd
daegyu94 <hdg9400@gmail.com>
2021-07-17 03:10:14 +0000

    Add install ohmyzsh

--- zsh-config/require.sh
+++ zsh-config/require.sh
@@ -1,3 +1,4 @@
 sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
