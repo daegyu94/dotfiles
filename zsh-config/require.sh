f6fd45142108757d53b827342aaa27cdaea67d3e
daegyu94 <hdg9400@gmail.com>
2023-03-08 09:43:26 +0900

    update zsh-config

--- zsh-config/require.sh
+++ zsh-config/require.sh
@@ -1,4 +1,7 @@
-sudo apt install -y zsh
+## first install zsh and change default shell to zsh 
+# sudo apt install -y zsh 
+# chsh -s /bin/zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
